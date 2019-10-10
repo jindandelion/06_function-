99c1796dc0653d73edcde4bc5b02a108c46e0854
jindandelion <gieoun524@sookmyung.ac.kr>
2019-10-10 16:13:27 +0900

    06_function

--- main.c
+++ main.c
@@ -2,19 +2,34 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-	
-void print_star()
+
+int sumTwo(int a,int b)
+
+int square(int n)
 {
-	int i;
-	
-	for(i=0;i<10;i++)
-		printf("*");
+	return (n*n);
+}
+int get_max(int a,int b)
+{
+	if(a>b)return(a);
+	else return(b);
 }
 int main(int argc, char *argv[]) {
 	
-	print_star();
-	print_star();
-	print_star();
-		
-	return 0;
+	int a, b;
+	int output;
+	
+	a=2;
+	b=5;
+	
+	output= sumTwo(a,b);
+	printf("sumTwo:%i\n", output);
+	
+	output= square(a);
+	printf("square:%i\n", output);
+	/*Same >>printf("square:%i\n", square(a));*/
+	output= get_max(a,b);
+	printf("get_max:%i\n", output);
+
+		return 0;
 }
