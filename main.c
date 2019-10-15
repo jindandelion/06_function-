c47d8fad51d4fd85ae1f849faf15b3aeb1ef61d2
jindandelion <gieoun524@sookmyung.ac.kr>
2019-10-15 16:08:24 +0900

    function_1015

--- main.c
+++ main.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int sumTwo(int a,int b)
-
-int square(int n)
+int factorial(int n)
 {
-	return (n*n);
+	int i;
+	int res=1;
+	for(i=1;i<=n;i++)
+	{
+		res=i*res;
+	}
+		
+	return res;
 }
-int get_max(int a,int b)
-{
-	if(a>b)return(a);
-	else return(b);
-}
-int main(int argc, char *argv[]) {
-	
-	int a, b;
-	int output;
-	
-	a=2;
-	b=5;
-	
-	output= sumTwo(a,b);
-	printf("sumTwo:%i\n", output);
-	
-	output= square(a);
-	printf("square:%i\n", output);
-	/*Same >>printf("square:%i\n", square(a));*/
-	output= get_max(a,b);
-	printf("get_max:%i\n", output);
 
-		return 0;
+int calCombination(int n, int r)
+{
+	int bunja,bunmo;
+	bunja=factorial(n);
+	bunmo=factorial(n-r)*factorial(r);
+	
+	return (bunja/bunmo);
+}
+
+int main(void)
+{
+	int n, r;
+	int res;
+	
+	printf("input n and r");
+	scanf("%d %d",&n,&r);
+	
+	res=calCombination(n, r);/*combination을 계산해주는 함수를 호출*/
+	printf("Combination result is %d\n", res);
+	
+	return 0;
 }
